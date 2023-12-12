8f91218187212c194124899afd69d64c391bff36
Saidumarkhan <113846412+Saidumarkhan@users.noreply.github.com>
2023-12-12 19:14:27 +0500

    change

---
+++ index.js
@@ -0,0 +1,22 @@
+// 001
+// let a = 5;
+// P = 4 * a;
+// console.log(P); 
+
+// 002
+// let a = 8;
+// S = a**2;
+// console.log(S);
+
+// 003
+// let a = 3;
+// let b = 2;
+// let S = a * b;
+// let P = 2 * (a+b);
+// console.log(S, P);
+
+// 004
+let P = 3.14;
+let d = 4;
+let L = P * d;
+console.log(L);
---
+++ index.py
@@ -0,0 +1,61 @@
+# 001
+# a = int(input("Kvadratning tomoni: "))
+# P = 4 * a
+# print(P)
+
+# 002
+# a = int(input("Kvadratning tomoni: "))
+# S = a ** 2
+# print(S)
+
+# 003
+# a = int(input("a  tomoni: "))
+# b = int(input("b tomoni: "))
+# S = a * b
+# P = 2 * (a + b)
+# print(S, P)
+
+# 004
+# p = 3.14
+# d = int(input("diametr: "))
+# L = p * d
+# print(L)
+
+# 005
+# a = 3
+# V = a**3
+# S = 6 * (a**2)
+# print(V, S)
+
+# 006
+# a = 3
+# b = 4
+# c = 3
+# V = a * b * c
+# S = 2 *((a * b)+(b * c)+(a * c))
+# print(V, S)
+
+# 007
+# R = 6
+# p = 3.14
+# L = 2 * p * R
+# S = p * R**2
+# print(L,S)
+
+# 008
+# a = 4
+# b = 8
+# A = (a+b)/2
+# print(A)
+
+# 009
+# import math
+# a  = 9
+# b = 9
+# S = math.sqrt(a*b)
+# print(S)
+
+# 010
+
+a = 3
+b = 4
